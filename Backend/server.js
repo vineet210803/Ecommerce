a00fc038126e5400490db014b093ce6644135c1c
vineet210803 <vineet210803@gmail.com>
2025-09-14 01:26:17 +0530

    update backend

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// App config
+const app= express()
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/', (req, res)=>{
+   res.send ("api is working")
+})
+
+app.listen(port, ()=>console.log('Server is listening on PORT: '+ port))
+
+
