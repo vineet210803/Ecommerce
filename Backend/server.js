02bf57d72a05f77b4985f11ade15798f5b6cb322
vineet210803 <vineet210803@gmail.com>
2025-09-15 11:29:55 +0530

    cloudinary connected

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // App config
 const app= express()
 const port = process.env.PORT || 3000;
 connectDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
