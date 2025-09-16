53648f759eea3546b3062479b9b602c447765f8d
vineet210803 <vineet210803@gmail.com>
2025-09-16 16:35:14 +0530

    commited logic for add a product into DB

--- Backend/controllers/productsController.js
+++ Backend/controllers/productsController.js
@@ -54,7 +54,17 @@ const addProduct = async (req, res) => {
   }
 };
 
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success: true, products})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+};
 
 const removeProduct = async (req, res) => {};
 
